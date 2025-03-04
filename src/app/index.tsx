613ae5355e56c617b850247edf9bacc35b25ec6e
Raja Kulasekaran <raja.kulasekaran@medgenome.com>
2025-03-04 12:02:40 +0530

    inits

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,19 @@ import React, {useEffect} from 'react';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import Routers from '../routers/main';
+import { store, persistor } from '../store';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 
-const App = (props) => <Routers />
+const App = (props) => 
+<Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 
 const mapStateToProps = (state: any) => {
   return {
